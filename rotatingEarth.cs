7556de3b6bb073ae172ddb7b41a1e6f0e1031942
gingja <43117902+gingja@users.noreply.github.com>
2018-09-09 16:18:28 -0230

    added 1 script
    Rotate spherical body script

---
+++ rotatingEarth.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotatingEarth : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.up, 55 * Time.deltaTime);
+
+		transform.RotateAround (Vector3.forward, Vector3.down, 35 * Time.deltaTime);
+	}
+}
